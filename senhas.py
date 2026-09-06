6d0e8bcdbc7296872398c7e969a651df1bd2258b
edukp16 <edu.korpe@gmail.com>
2026-09-06 14:06:20 -0300

    Add password generator script in senhas.py

---
+++ senhas.py
@@ -0,0 +1,11 @@
+import random
+import time
+print('Bem vindo ao criador de senhas!')
+time.sleep(1)
+caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+tamanho_senha = int(input('Quantos caracteres sua senha terá?'))
+senha = ""
+for i in range(tamanho_senha):
+    senha += random.choice(caracteres)
+
+print('Sua senha gerada é: ', senha)
